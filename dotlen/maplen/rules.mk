2d913aded662da1f7ee3905a8897af2126e3e223
lental <me@michaellen.com>
2023-01-18 22:05:26 -0800

    move all firmware into the repo

---
+++ dotlen/maplen/rules.mk
@@ -0,0 +1,24 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+LTO_ENABLE = yes
+BACKLIGHT_ENABLE = no
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+CONSOLE_ENABLE = no  # Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
+ENCODER_ENABLE = yes
+
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
